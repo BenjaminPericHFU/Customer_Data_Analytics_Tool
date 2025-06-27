2810cf059ce2a3b03f2029cdb23e5379448eab4d
BenjaminPericHFU <161472721+BenjaminPericHFU@users.noreply.github.com>
2025-06-27 12:22:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,19 @@ import numpy as np
 
 st.title('Customer Data Analytics Tool')
 
+tabs = sst.tabs(["Cat", "Dog", "Owl"], width="stretch")
+
 # File uploader – supports CSV and Excel files
-"""uploaded_file = st.file_uploader("Input Customer Data: ", type=["csv", "xlsx", "xls"])
+#"""uploaded_file = st.file_uploader("Input Customer Data: ", type=["csv", "xlsx", "xls"])
 
-if uploaded_file is not None:
-    if uploaded_file.name.endswith(".csv"):
-        df = pd.read_csv(uploaded_file)
-    else:
-        df = pd.read_excel(uploaded_file, engine='openpyxl')
+#if uploaded_file is not None:
+#    if uploaded_file.name.endswith(".csv"):
+#        df = pd.read_csv(uploaded_file)
+#    else:
+#        df = pd.read_excel(uploaded_file, engine='openpyxl')
+#
+#st.dataframe(pd.DataFrame(df.columns, columns=["Column Names"]))
 
-st.dataframe(pd.DataFrame(df.columns, columns=["Column Names"]))
-"""
 st.title("Custom or Uploaded Data Loader")
 
 # Checkbox: use custom data or not
